115c54506de5d4cf83a7f72323c86a0db904711f
gdario <git.gdario@gmail.com>
2022-05-08 19:02:02 +0200

    remove transpiled files

--- test/infra/infra-mocks.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MockDatabase = void 0;
-class MockDatabase {
-    constructor() {
-        this.couriers = new Map();
-    }
-}
-exports.MockDatabase = MockDatabase;
